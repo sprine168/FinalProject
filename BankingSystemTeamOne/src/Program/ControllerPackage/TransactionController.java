dcc1f2c2bfd2221d50b15b8895490edd0af42852
Steven Prine <sprine@missouriwestern.edu>
2018-12-02 01:40:26 -0600

    Added transaction pages for the gui. Also, I added the controlled with variables and actionEvents to be used with the backend.

---
+++ BankingSystemTeamOne/src/Program/ControllerPackage/TransactionController.java
@@ -0,0 +1,42 @@
+package Program.ControllerPackage;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
+/*
+    TransactionController is used for obtaining values from the CreateTransactions.fxml page. The controller should
+    pass content to savings account class.
+*/
+public class TransactionController {
+
+    // TODO create the rest of the controller to be in lieu of the pending transaction class.
+
+    // Customer selection variables
+    public ComboBox selectAccountType;
+    public ComboBox selectCustomer;
+
+    // Credit card input variables
+    public TextField creditName;
+    public TextField creditAmount;
+    public Button submitCredit;
+
+    // Checking input variables
+    public TextField checkName;
+    public TextField checkAmount;
+    public Button submitCheck;
+
+    // Checking variables
+
+
+
+
+    // returnCreditTransaction is used to submit the credit card transaction.
+    public void returnCreditTransaction(ActionEvent actionEvent) {
+    }
+
+    // returnCheckTransaction is used to submit the checking transactions.
+    public void returnCheckTransaction(ActionEvent actionEvent) {
+    }
+}
